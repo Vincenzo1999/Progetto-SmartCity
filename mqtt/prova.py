66dd1206a281742376906f64374870c7357a1c7f
vincenzo <vincverd99@gmail.com>
2024-10-24 11:43:01 +0200

    modifiche sumo per la simulazione

--- mqtt/prova.py
+++ mqtt/prova.py
@@ -1,50 +1,70 @@
 import traci
-import random
 import sumolib
-import subprocess
+import random
+import time
+
+import traci.exceptions
 
 # Percorso al file di configurazione SUMO
 path = "/home/vincenzo/simulazione _osm/simulazione.sumocfg"
-traci.start(["sumo-gui", "-c", path, "--step-length", "1"])
 
-# Leggi la rete
-#net = sumolib.net.readNet("/home/vincenzo/Sumo/2024-10-17-17-04-06/osm.net.xml")
+# Avvia la simulazione
+traci.start(["sumo", "-c", path, "--step-length", "1"])
+vehicle_routes = {}
+for vehicle in sumolib.xml.parse("/home/vincenzo/simulazione _osm/simulazione.rou.xml", "vehicle"):
+    route = vehicle.route[0]  # Accesso al primo (e unico) elemento 'route'
+    edges = route.edges.split()  # Ottieni la lista degli edges della route
+    vehicle_routes[vehicle.id] = edges  # Aggiungi il veicolo e la sua route al dizionario
+veicolo_id="1"#random.choice(list(vehicle_routes.keys()))
+def route_update(vehicle_id,end_edge):
+    new_destination_list=[]
+    for other_vehicle_id, other_route in vehicle_routes.items():
+      if other_vehicle_id != vehicle_id and end_edge in other_route:
+                        # Trova l'indice dell'edge corrente nella route
+        index = other_route.index(end_edge)
+                        # Verifica se esiste un edge successivo
+        if index + 1 < len(other_route):
+          next_edge = other_route[index + 1]
+                            # Aggiorna la route del veicolo corrente
+          new_destination_list.append(next_edge)
+        else:
+          print("errore index")    # Aggiungi il prossimo edge alla route
+    new_destination= random.choice(new_destination_list)
+    #traci.vehicle.changeTarget(veicolo_id,new_destination)
+    return new_destination 
 
-
-
-while traci.simulation.getMinExpectedNumber() > 0:
-  traci.simulationStep()
-# Ottieni la lista delle corsie e crea una lista degli edge per i veicoli passeggeri
-  lane_list = traci.lane.getIDList()
-  edge_list = []
-
-  for lane in lane_list:
+step=0
+while step<1000:
+ traci.simulationStep()
+ try: 
+    current_edge = traci.vehicle.getRoadID(veicolo_id)
+    route= traci.vehicle.getRoute(veicolo_id)
+    emission = traci.vehicle.getCO2Emission(veicolo_id)
     
-    allowed_vehicles = traci.lane.getAllowed(lane)
-    if 'passenger' in allowed_vehicles:
-        edge = traci.lane.getEdgeID(lane)
-        edge_list.append(edge)
-
-# Seleziona casualmente un edge di partenza e uno di arrivo
-  start_edge = random.choice(edge_list)
-  end_edge = random.choice(edge_list)
-
-# Esegui findAllRoutes.py per ottenere la rotta
-  cmd = ["python3", "findAllRoutes.py", "-n", path, "-s", start_edge, "-t", end_edge]
-  result = subprocess.run(cmd, capture_output=True, text=True)
-
-# Stampa l'output del comando
-  print("Output di findAllRoutes.py:", result.stdout)
-    # Ottieni la lista dei veicoli nella simulazione
-  veicoli = traci.vehicle.getIDList()
-
-  if veicoli:  # Verifica se ci sono veicoli nella simulazione
-        # Seleziona casualmente un veicolo
-        veicolo = random.choice(veicoli)
+    print(emission,route)
+    if current_edge==route[-1]:
+       next_edge = route_update(veicolo_id,current_edge)
+       
+       
+       route_find = traci.simulation.findRoute(current_edge,next_edge)
+       if route_find.edges != "":
+        new_route=[]
+        for edge in route_find.edges:
+          new_route.append(edge)
+        print(f"new_route:{new_route}")  
+        traci.vehicle.setRoute(veicolo_id,new_route)
+       else:
+        print("non ci sono più edge da aggiungere") 
         
-        # Verifica l'output di findAllRoutes.py e imposta la nuova rotta
-        new_route = result.stdout.strip().split("\n")
-        if new_route and new_route[0] != "":
-            traci.vehicle.setRoute(veicolo, new_route)
+       
+
+    step += 1   
+ except traci.exceptions.TraCIException: 
+  print(f"{step} ciao")
+  time.sleep(1)
+  step += 1
+
+ 
 
 traci.close()
+
